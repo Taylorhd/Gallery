8705fc7cc4d860a206b54fbe70850e8e8c8f9cef
e <2469458627@qq.com>
2020-02-04 21:09:56 +0800

    先简单看看kotlin再来撸代码

--- app/src/main/java/com/ecut/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/ecut/gallery/GalleryViewModel.kt
@@ -2,11 +2,16 @@ package com.ecut.gallery
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 
 class GalleryViewModel(application: Application) : AndroidViewModel(application) {
     private val _photoListLive = MutableLiveData<List<PhotoItem>>()
+   val photoListLive :LiveData<List<PhotoItem>>
     get() = _photoListLive
+
+
+
 
 }
 
---
+++ app/src/main/java/com/ecut/gallery/VolleySingleton.kt
@@ -0,0 +1,18 @@
+package com.ecut.gallery
+
+import android.content.Context
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
+
+class VolleySingleton private constructor(context: Context) {
+    companion object {
+        private var INSTANCE :VolleySingleton?=null
+        fun getInstance(context: Context)={
+            VolleySingleton(context).also { INSTANCE = it }
+        }
+    }
+
+    val requestQueue :RequestQueue by lazy {
+        Volley.newRequestQueue(context.applicationContext)
+    }
+}
